c0e22eb11a2a4febff63d1f77425e8907619348b
YuliiaaLevchenko <141173167+YuliiaaLevchenko@users.noreply.github.com>
2024-02-18 22:45:39 +0200

    localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Description from "../Description/Description";
 import Feedback from "../Feedback/Feedback";
@@ -7,8 +8,17 @@ import Notification from "../Notification/Notification";
 
 const initialState = { good: 0, neutral: 0, bad: 0 };
 
+const getInitialFeedback = () => {
+  const savedFeedback = window.localStorage.getItem('feedback');
+  return savedFeedback !== null ? JSON.parse(savedFeedback) : initialState;
+};
+
 function App() {
-  const [feedback, setFeedback] = useState(initialState);
+  const [feedback, setFeedback] = useState(getInitialFeedback);
+
+  useEffect(() => {
+    window.localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   const updateFeedback = (key) => {
     setFeedback({
